4c99ec29cc251160bed70a1fbc10e173b7d29f49
lyang1024 <lqyang210@gmail.com>
2016-02-05 15:34:38 -0800

    program

---
+++ Command.h
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class Command{
+    protected:
+        string cmd;
+    public:
+        virtual void parse();
+        virtual void run();
+}
+
+class SingleCom{
+    private:
+        vector<
+class MultiCom{
+    private:
+        vector<
