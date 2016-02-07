49581913aa2a8e97aa85c978b1e9e9b4d51159f9
lyang1024 <lqyang210@gmail.com>
2016-02-07 14:54:57 -0800

    command class

--- Command.h
+++ Command.h
@@ -3,15 +3,43 @@ using namespace std;
 
 class Command{
     protected:
-        string cmd;
+        char cmd[1000];
+        char *tokens[10];
     public:
         virtual void parse();
-        virtual void run();
+        virtual void execute();
 }
 
-class SingleCom{
-    private:
-        vector<
+class SingleCom:public Command{
+    public:
+    void parse(){
+        if(strcmp(cmd,"exit")==0) exit(0);
+        tokens=strtok(cmd," ");
+        while(tokens!=NULL)
+        {
+            ++tokens;
+            tokens=strtok(NULL," ");
+        }
+        return 0;
+    }
+    void execute()
+    {
+        pid_t pid;
+        int status;
+        if((pid=fork())<0){
+            printf("ERROR: Forking child process failure\n");
+            exit(1);
+        }
+        else if(pid==0){
+            if(execvp(*tokens,token)<0){
+                printf("ERROR:Execution failure\n");
+                exit(1);
+            }
+        }
+        else{
+            while
+
+
 class MultiCom{
     private:
         vector<
