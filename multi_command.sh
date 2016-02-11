fc97ccd0481b75d6edacbee2dda615d01d1d1978
0.0 <ZX@ucrwpa2-4-10-25-45-76.wnet.ucr.edu>
2016-02-11 15:50:24 -0800

    add multi_command.sh

---
+++ multi_command.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo "Test command 1 : ls -a && pwd"
+ls -a && pwd
+echo "Test command 2 : fail command && pwd"
+fail command && pwd
+echo "Test command 3 : ls -a || pwd"
+ls -a || pwd
+echo "Test command 4 : fail command || pwd"
+fail command || pwd
+echo "Test command 5 : ls -a; pwd"
+ls -a; pwd
+echo "Test command 6 : fail command; pwd"
+fail command; pwd
+echo "Test command 7 : cal; ls -a && pwd || echo world; echo hello"
+cal; ls -a && pwd || echo world; echo hello
+echo "Test command 8 : ls -a && fail command || pwd"
+ls -a && fail command || pwd
+echo "Test command 9 : fail command && ls -a || pwd"
+fail command && ls -a || pwd
+echo "Test command 10 :cal;echo hello&&mkdir test||echo world;git status"
+cal;echo hello&&mkdir test||echo world;git status
+ 
